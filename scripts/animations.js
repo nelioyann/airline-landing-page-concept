035955e11b2e3138c7bbedad237d4104677c42b0
nelioyann <nanayannick1998@gmail.com>
2020-04-30 11:59:51 +0200

    ed

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,4 +1,4 @@
-var globe_tl = gsap.timeline({ repeat: -1, repeatDelay: 3, defaults: { duration: 1, ease: "power3.Out" } });
+var globe_tl = gsap.timeline({ repeat: -1, repeatDelay: 10, defaults: { duration: 1, ease: "power3.Out" } });
 var form_tl = gsap.timeline({ paused: true, reversed: true, defaults: { duration: 0.3, ease: "power3.Out" } });
 var overlay_tl = gsap.timeline({ repeat: -1, repeatDelay: 0, defaults: { duration: 3, ease: "power3.Out" } });
 
@@ -13,7 +13,7 @@ form_tl
     .to(".hero_title", {autoAlpha: 0, y: -20})
     .to(".hero_button", {autoAlpha: 0, y: -20})
     .to(".trip_form", 1,{ autoAlpha: 1, y: "0vh"})
-    .to(".logo", 2, {left: 300}, "-=1")
+    .to(".logo", 2, {left: "30vh"}, "-=1")
     .to(".logoBrand", 1, {color: "#000"})
     .to(".form_people", {autoAlpha: 1}, "-=1")
     
