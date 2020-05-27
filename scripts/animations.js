cefddc879b83bb42bd41c44af5154564f116bbb1
nelioyann <nanayannick1998@gmail.com>
2020-05-27 10:37:14 +0200

    go

--- scripts/animations.js
+++ scripts/animations.js
@@ -5,15 +5,18 @@ var overlay_tl = gsap.timeline({ repeat: -1, repeatDelay: 0, defaults: { duratio
 gsap.to(".trip_form", 0.2, {autoAlpha: 0, y: "100vh"})
 gsap.to(".form_fields", 0.2, {autoAlpha: 0})
 gsap.to(".overlay", {autoAlpha: 0})
-
+gsap.to(".form_fields *", 1, {autoAlpha: 0})
 
 overlay_tl
 .to('.wheel_img', {rotation: 360})
+
 form_tl
-    .to(".hero_title", {autoAlpha: 0, y: -20})
-    .to(".hero_subtitle", {autoAlpha: 0, y: -20})
-    .to(".hero_button", {autoAlpha: 0, y: -20})
+.to(".hero_overlay", {autoAlpha: 0})
+.to(".hero_title", {autoAlpha: 0, y: -20})
+    .to(".hero_subtitle", {autoAlpha: 0, y: -20}, "-=0.2")
+    .to(".hero_button", {autoAlpha: 0, y: -20}, "-=0.2")
     .to(".trip_form", 1,{ autoAlpha: 1, y: "0vh"})
+    .to(".form_fields *", 1, {autoAlpha: 1})
     .to(".logoBrand", {color: "#000"}, "-=1")
     .to(".form_people", {autoAlpha: 1}, "-=1")
     
